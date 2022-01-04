bf7696e71da31ad21118e3373e0f25f738ab8c88
ldelossa <louis.delos@gmail.com>
2022-01-04 10:12:32 -0500

    fix: restore window in on_resize autocmd
    before this commit the on_resize autocommand left the Neovim UI inside
    a litee panel window. This is incorrect and now it sets the current
    window to the window before the toggle takes place.
    
    Signed-off-by: ldelossa <louis.delos@gmail.com>

--- lua/litee/lib/panel/autocmds.lua
+++ lua/litee/lib/panel/autocmds.lua
@@ -4,12 +4,15 @@ local lib_panel = require('litee.lib.panel')
 local M = {}
 
 function M.on_resize()
+    local cur_win = vim.api.nvim_get_current_win()
     local cur_tab = vim.api.nvim_get_current_tabpage()
     local state = lib_state.get_state(cur_tab)
     if state == nil then
         return
     end
     lib_panel.toggle_panel(state, false, true)
+    -- restore window
+    vim.api.nvim_set_current_win(cur_win)
     vim.cmd("redraw!")
 end
 
